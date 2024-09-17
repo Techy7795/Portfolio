7367edd024e8fae5fe071142db9526b7ad93dd6d
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-09-17 16:31:06 +0530

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -86,7 +86,7 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://drive.google.com/file/d/1RhRcAc-7kvjGfHF4Y0gf6KGbH18Rm5tq/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1CqOEfQjq2PPC8RkKBm_8PAXAClpacrZx/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
